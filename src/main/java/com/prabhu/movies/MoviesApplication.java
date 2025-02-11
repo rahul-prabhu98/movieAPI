0774108349dd3f174b3f9441d5584626b17a48f8
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-02-11 13:48:31 -0500

    Setting up the environment variables

--- src/main/java/com/prabhu/movies/MoviesApplication.java
+++ src/main/java/com/prabhu/movies/MoviesApplication.java
@@ -2,6 +2,8 @@ package com.prabhu.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -9,5 +11,7 @@ public class MoviesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+
+
 
 }
