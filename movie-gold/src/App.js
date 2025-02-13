ae1c15af3b9a7e08bbcb4af20a5ff311c31d2295
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-02-13 16:28:38 -0500

    Created the trailer component with react player

--- movie-gold/src/App.js
+++ movie-gold/src/App.js
@@ -6,6 +6,7 @@ import Layout from './components/Layout.js';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home.js';
 import Header from './components/header/Header.js';
+import Trailer from './components/trailer/Trailer.js';
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
       const response = await api.get("/api/v1/movies");
       console.log(response.data)
       setMovies(response.data);
-    } 
+    }
     catch (err) {
       console.log(err);
     }
@@ -27,15 +28,14 @@ function App() {
 
   useEffect(() => {
     getMovies();
-  },[]);
+  }, []);
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Layout/>}>
-        <Route path='/' element={<Home movies={movies}/>}></Route>
-        
-
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home movies={movies} />}></Route>
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer />}></Route>
         </Route>
       </Routes>
     </div>
