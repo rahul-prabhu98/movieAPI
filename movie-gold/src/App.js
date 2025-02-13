b1050327fc4522ddaaaece749d8d6b7c61d8ce3c
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-02-13 16:58:24 -0500

    Created the reviews component

--- movie-gold/src/App.js
+++ movie-gold/src/App.js
@@ -7,12 +7,15 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home.js';
 import Header from './components/header/Header.js';
 import Trailer from './components/trailer/Trailer.js';
+import Reviews from './components/reviews/Reviews.js';
 
 function App() {
 
   window.process = process;
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews,setReviews] = useState();
 
   const getMovies = async () => {
 
@@ -26,6 +29,17 @@ function App() {
     }
   }
 
+  const getMovieData = async (movieId) => {
+    try{
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+      const singleMovie = response.data;
+      setMovie(singleMovie);
+      setReviews(singleMovie.reviews);
+    }catch(error){
+
+    }
+  }
+
   useEffect(() => {
     getMovies();
   }, []);
@@ -36,6 +50,7 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<Home movies={movies} />}></Route>
           <Route path='/Trailer/:ytTrailerId' element={<Trailer />}></Route>
+          <Route path='/Reviews/:movieId' element = {<Reviews getMovieData={getMovieData} reviews={reviews} setReviews={setReviews} /> }></Route>
         </Route>
       </Routes>
     </div>
